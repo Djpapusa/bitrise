ba4de6af2fe29a0802229062530ba1a1fcd8154d
trapacska <trapacska@gmail.com>
2018-01-12 14:39:00 +0100

    tests fixed

--- bitrise/print_test.go
+++ bitrise/print_test.go
@@ -217,7 +217,7 @@ func TestGetRunningStepFooterMainSection(t *testing.T) {
 		}
 
 		actual := getRunningStepFooterMainSection(result)
-		expected := "| \x1b[32;1m✓\x1b[0m | \x1b[32;1m\x1b[0m                                                              | 999+ hour|"
+		expected := "| \x1b[32;1m✓\x1b[0m | \x1b[32;1m(0)                          (…, this is a very long string.)\x1b[0m | 999+ hour |"
 		require.Equal(t, expected, actual)
 	}
 }
